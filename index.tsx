c988f7174678a81a230b518c0aaeeda820a05969
akbarshah.com5 <akbarshah.com5@gmail.com>
2025-12-23 12:14:30 +0000

    fix: redirect /tools to homepage to resolve content issue

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 import Home from './Home';
 import ToolPage from './ToolPage';
@@ -21,7 +21,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="tools" element={<Home />} />
+          <Route path="tools" element={<Navigate to="/" replace />} />
           <Route path="tools/:slug" element={<ToolPage />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
